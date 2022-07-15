3ead6783af89a99f129575f6fcb2c370d40c3185
HyperPaint <dioman172@yandex.ru>
2022-07-15 20:14:32 +1000

    Initial commit

---
+++ ECC.cpp
@@ -0,0 +1,189 @@
+﻿#include <cmath>
+#include <vector>
+
+using namespace std;
+
+struct Curve
+{
+    // числа, определяющие кривую
+    int a;
+    int b;
+    // определяет длину осей
+    int mod;
+};
+
+// глобальная переменная определяющая кривую
+Curve curve;
+
+struct Point
+{
+    int x;
+    int y;
+};
+
+/* работа с кривыми */
+template <typename T>
+T normalize(T a);
+int inverse(int a);
+Point add(Point first, Point second);
+Point sub(Point first, Point second);
+Point multiply(Point point, int multiplier);
+bool setCurve(int a, int b, int mod);
+bool checkCurve();
+vector<Point> getAllPoints();
+
+// mod
+template <typename T>
+T normalize(T a)
+{
+    while (a < 0)
+    {
+        a += curve.mod;
+    }
+    while (a >= curve.mod)
+    {
+        a -= curve.mod;
+    }
+    return a;
+}
+
+// возвращает обратный элемент
+int inverse(int a)
+{
+    if (a == 0)
+    {
+        return a;
+    }
+    else
+    {
+        for (int i = 0; i < curve.mod; i++)
+        {
+            if (((a * i) % curve.mod) == 1)
+            {
+                return i;
+            }
+        }
+    }
+    throw 0;
+}
+
+// сложение двух точек
+Point add(Point first, Point second)
+{
+    Point result;
+    // числитель, знаменатель, угол наклона прямой
+    int top, bottom, tangent;
+    // одна и та же точка
+    if (first.x == second.x && first.y == second.y)
+    {
+        top = normalize(3 * (int)pow(first.x, 2) + curve.a);
+        bottom = inverse(normalize(2 * first.y));
+        tangent = normalize(top * bottom);
+        result.x = normalize((int)pow(tangent, 2) - first.x - second.x);
+        result.y = normalize(tangent * (first.x - result.x) - first.y);
+        return result;
+    }
+    // одна пара
+    else if (first.x == second.x)
+    {
+        // бесконечно удаленная точка
+        result.x = -1;
+        result.y = -1;
+        return result;
+    }
+    // первая точка бесконечно удаленная
+    else if (first.x == -1 && first.y == -1)
+    {
+        return second;
+    }
+    // вторая точка бесконечно удаленная
+    else if (second.x == -1 && second.y == -1)
+    {
+        return first;
+    }
+    // разные точки
+    else
+    {
+        top = normalize(first.y - second.y);
+        bottom = inverse(normalize(first.x - second.x));
+        tangent = normalize(top * bottom);
+        result.x = normalize((int)pow(tangent, 2) - first.x - second.x);
+        result.y = normalize(tangent * (first.x - result.x) - first.y);
+        return result;
+    }
+}
+
+// вычитание
+Point sub(Point first, Point second)
+{
+    second.y = normalize(-second.y);
+    return add(first, second);
+}
+
+// умножение
+Point multiply(Point point, int multiplier)
+{
+    Point Result = point;
+    for (int i = 1; i < multiplier; i++)
+    {
+        Result = add(Result, point);
+    }
+    return Result;
+}
+
+// задать кривую
+bool setCurve(int a, int b, int mod)
+{
+    curve.a = a;
+    curve.b = b;
+    curve.mod = mod;
+    // проверка кривой на сингулярность
+    return checkCurve();
+}
+
+// проверка кривой
+bool checkCurve()
+{
+    if (4 * (int)pow(curve.a, 3) + 27 * (int)pow(curve.b, 2) == 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+// нахожу все точки кривой (целочисленные)
+vector<Point> getAllPoints()
+{
+    vector<Point> points;
+    Point point;
+    int value;
+    // перебираю все возможные точки от 0 до максимального значения в остатках
+    for (int current = 1; current < curve.mod; current++)
+    {
+        // по формуле кривой вычисляю y
+        value = normalize((int)pow(current, 3) + curve.a * current + curve.b);
+        // корень в остатках
+        // перебираю все числа от 0 до максимального значения в остатках
+        for (int i = 0; i < curve.mod; i++)
+        {
+            // если число в квадрате == текущему значению, то оно и будет результатом корня
+            if (i * i % curve.mod == value)
+            {
+                // записываю точку
+                point.x = current;
+                point.y = i;
+                points.push_back(point);
+                // и обратную ей по y точку
+                point.x = current;
+                point.y = normalize(curve.mod - i);
+                points.push_back(point);
+                // выхожу из цикла
+                break;
+            }
+        }
+    }
+    return points;
+}
---
+++ ECDSA.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+#include "ECC.cpp" // кривые
+#include "SHA1.cpp" // хэширование
+
+using namespace std;
+
+string getTextFromFile(string path);
+int inverse(int a, int base);
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    // рандомизация рандома
+    srand(time(0));
+
+    // вот это буду подписывать
+    string message = getTextFromFile("file.txt");
+
+    // вот такая кривая
+    int a = 1;
+    int b = 6;
+    int mod = 11;
+    cout << "Кривая с параметрами a: " << a << " b: " << b << " mod: " << mod << endl;
+    cout << "Подходит: " << boolalpha << setCurve(1, 6, 11) << endl;
+    
+    // все точки кривой
+    vector<Point> allPoints = getAllPoints();
+    // первообразный корень
+    Point pointP = allPoints[rand() % allPoints.size()];
+    cout << "Первообразный корень: " << "(" << pointP.x << " " << pointP.y << ")" << endl;
+    // вычисляю порядок подгруппы
+    cout << "Точки циклической группы:" << endl;
+    Point pointPcopy = pointP;
+    int q = 0; // порядок циклической подгруппы - большое простое число - количество точек в группе
+    while (true)
+    {
+        q++;
+        pointPcopy = add(pointPcopy, pointP);
+        if (pointP.x == pointPcopy.x && pointP.y == pointPcopy.y)
+        {
+            break;
+        }
+        cout << "(" << pointPcopy.x << " " << pointPcopy.y << ")" << endl;
+    }
+    cout << "Порядок группы: " << q << endl; // если число не простое, не будет обратного элемента
+
+    // генерация ключей
+    int x = (rand() % (q - 1)) + 1; // закрытый ключ
+    cout << "Закрытый ключ: " << x << endl;
+    Point pointQ = multiply(pointP, x); // открытый ключ
+    cout << "Открытый ключ: " << "(" << pointQ.x << " " << pointQ.y << ")" << endl;
+
+    // подпись
+    start:
+    int k = (rand() % (q - 1)) + 1; // случайное целое число
+    int r = multiply(pointP, k).x % q;
+    if (r == 0)
+    {
+        goto start; // уравнение подписи не зависит от секретного ключа
+    }
+    int s = (inverse(k, q) * (stoll(myHash(message), 0, 16) + x * r)) % q;
+    if (s == 0)
+    {
+        goto start; // обратного значения нет, невозможно проверить
+    }
+    cout << "Реальная подпись: " << "(" << r << " " << s << ")" << endl; // это подпись
+
+    // проверка
+    int u1 = (inverse(s, q) * stoll(myHash(message), 0, 16)) % q;
+    int u2 = (inverse(s, q) * r) % q;
+    Point checkPoint = add(multiply(pointP, u1), multiply(pointQ, u2));
+    checkPoint.x = checkPoint.x % q;
+    cout << "Вычисленная подпись: " << "(" << checkPoint.x << " " << "X" << ")" << endl; // это вычисленная подпись
+    cout << "Подходит: " << ((checkPoint.x % q) == r) << endl;
+
+    system("pause");
+    return 0;
+}
+
+string getTextFromFile(string path)
+{
+    // считывание файла
+    ifstream file;
+    // открытие файла в бинарном виде на чтение
+    string text = "";
+    file.open(path, ios::in | ios::binary);
+    if (!file.is_open())
+    {
+        file.close();
+        return "";
+    }
+    // считываю файл
+    const size_t readCount = 100;
+    char buff[readCount] = "\0";
+    while (!file.eof())
+    {
+        file.read(buff, readCount);
+        text += buff;
+    }
+    // закрываю его
+    file.close();
+
+    return text;
+}
+
+// возвращает обратный элемент
+int inverse(int a, int base)
+{
+    if (a == 0)
+    {
+        return a;
+    }
+    else
+    {
+        for (int i = 0; i < base; i++)
+        {
+            if (((a * i) % base) == 1)
+            {
+                return i;
+            }
+        }
+    }
+    throw 0;
+}
